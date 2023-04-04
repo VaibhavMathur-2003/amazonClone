7f5829f582be927c22a0a75f7e522dab62d6eef9
VaibhavMathur03 <mathurvaibhav010@gmail.com>
2023-04-04 20:19:12 +0530

    bug fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,19 +11,19 @@ export const Navbar = () => {
       <MediaQuery minWidth={850}>
         <nav className="navbar container-fluid">
           <Link to="/">
-          <div className="logo">
-            <img
-              src="https://bizmonthly.com/wp-content/uploads/2020/04/amazon-logo-black.png"
-              alt="nhi mil rahi"
-            />
-          </div>
+            <div className="logo">
+              <img
+                src="https://bizmonthly.com/wp-content/uploads/2020/04/amazon-logo-black.png"
+                alt="nhi mil rahi"
+              />
+            </div>
           </Link>
           <div className="navlist">
             <div className="option">
               Hello
               <span>Guest</span>
             </div>
-            
+            <a href="https://courageous-shortbread-fd0286.netlify.app/">
             <div className="option">
               <img
                 src="https://play-lh.googleusercontent.com/Uyk7DkQElC-0OfJCOHZLIgnQ0ulHoEC06d9ENRklNh-0t6nrFc6mqmpRMajoVU3Yt9w"
@@ -31,16 +31,17 @@ export const Navbar = () => {
                 style={{ height: "70px", width: "60px" }}
               />
             </div>
+            </a>
             <Link to="/cart">
-            <div className="option">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
-                alt=""
-              />
-              <span>
-                <strong>{cart?.length}</strong>
-              </span>
-            </div>
+              <div className="option">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+                  alt=""
+                />
+                <span>
+                  <strong>{cart?.length}</strong>
+                </span>
+              </div>
             </Link>
 
             <div className="option">
@@ -48,31 +49,30 @@ export const Navbar = () => {
               <span>0</span>
             </div>
           </div>
-
         </nav>
       </MediaQuery>
       <MediaQuery maxWidth={850}>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-          <Link to="/">
-            <div className="navbar-brand logo">
-              <img
-                src="https://bizmonthly.com/wp-content/uploads/2020/04/amazon-logo-black.png"
-                alt="nhi mil rahi"
-              />
-            </div>
+            <Link to="/">
+              <div className="navbar-brand logo">
+                <img
+                  src="https://bizmonthly.com/wp-content/uploads/2020/04/amazon-logo-black.png"
+                  alt="nhi mil rahi"
+                />
+              </div>
             </Link>
             <Link to="/cart">
-            <div className="nav-item option">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
-                alt=""
-                style={{ height: "30px" }}
-              />
-              <span>
-                <strong>{cart?.length}</strong>
-              </span>
-            </div>
+              <div className="nav-item option">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+                  alt=""
+                  style={{ height: "30px" }}
+                />
+                <span>
+                  <strong>{cart?.length}</strong>
+                </span>
+              </div>
             </Link>
             <button
               className="navbar-toggler"
@@ -97,16 +97,17 @@ export const Navbar = () => {
                   </div>
                 </li>
                 <li className="nav-item">
-                  <div className="nav-link option">
-                    <img
-                      src="https://img.icons8.com/color/2x/amazon-prime-video.png"
-                      alt=""
-                      style={{ height: "70px", width: "60px" }}
-                    />
-                  </div>
+                  <a href="https://courageous-shortbread-fd0286.netlify.app/">
+                    <div className="nav-link option">
+                      <img
+                        src="https://img.icons8.com/color/2x/amazon-prime-video.png"
+                        alt=""
+                        style={{ height: "70px", width: "60px" }}
+                      />
+                    </div>
+                  </a>
                 </li>
               </ul>
-              
             </div>
           </div>
         </nav>
