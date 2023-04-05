63ba0f630aeb64117e154045fb28e59377de83b7
VaibhavMathur03 <mathurvaibhav010@gmail.com>
2023-04-05 18:50:40 +0530

    carousel change

--- src/Carousal.js
+++ src/Carousal.js
@@ -1,40 +1,37 @@
 import React from "react";
-import Carousel from "react-elastic-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-export const Carousal = () => {
-  return (
-    <>
-      <Carousel
-        emulateTouch={true}
-        useKeyboardArrows={true}
-        stopOnHover={true}
-        dataTouch={true}
-        enableAutoPlay={true}
-        autoPlaySpeed={5000}
-      >
-        <div className="carouselImage">
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+<div className="carouselImage"  onDragStart={handleDragStart}>
           <img
             src="https://m.media-amazon.com/images/I/71xanPwQm2L._SX3000_.jpg"
             alt="Na"
             style={{ height: "auto", width: "100%" }}
           />
-        </div>
-
-        <div className="carouselImage">
+        </div>,
+        <div className="carouselImage"  onDragStart={handleDragStart}>
           <img
             src="https://wallpapers.com/images/featured/npcp6jc782ixp9zs.jpg"
             alt="Na"
             style={{ height: "auto", maxHeight: "530px", width: "100%" }}
           />
-        </div>
-        <div className="carouselImage">
+          </div>,
+<div className="carouselImage"  onDragStart={handleDragStart}>
           <img
             src="https://m.media-amazon.com/images/G/31/prime/detail_page/Prime_Detail_Page_PC_VideoLaunch_1344x526._CB417703432_.jpg"
             alt="Na"
             style={{ height: "auto", width: "100%" }}
           />
-        </div>
-      </Carousel>
+        </div>];
+export const Carousal = () => {
+  return (
+    <>
+    <AliceCarousel autoPlay={true} autoPlayInterval={2000} infinite={true} disableButtonsControls={true} mouseTracking items={items} />
+      
     </>
   );
 };
--- src/Shopping.js
+++ src/Shopping.js
@@ -4,47 +4,48 @@ import { ShoppingCard } from "./ShoppingCard";
 import Shoes from "./Shoes.json";
 import Chocolates from "./Chocolates.json";
 import Tech from "./Tech.json";
-import Carousel from "react-elastic-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
-
-const breakPoints = [
-  { width: 1, itemsToShow: 1},
-  { width: 350, itemsToShow: 2},
-  { width: 610, itemsToShow: 3},
-  { width: 1080, itemsToShow: 4},
-  
-];
-
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 3 },
+};
+const item1 = Tech.map((i) => {
+  return (<ShoppingCard key={i.id} {...i} />);
+})
+const item2 = Shoes.map((i) => {
+  return (<ShoppingCard key={i.id} {...i} />);
+})
+const item3 = Chocolates.map((i) => {
+  return (<ShoppingCard key={i.id} {...i} />);
+})
 export const Shopping = () => {
   return (
     <>
-    
       <div className="shoes">
-        <h2 className="items">Stunning Luxurious Shoes</h2>
-        <Carousel className="shoeCaro" breakPoints={breakPoints} outerSpacing={0} emulateTouch={true} useKeyboardArrows={true} stopOnHover={false} dataTouch={true}>
-          {Tech.map((i) => {
-            return <ShoppingCard key = {i.id} {...i} />;
-          })}
-        </Carousel>
+        <h2 className="items">Tech Products</h2>
+        <AliceCarousel 
+        responsive={responsive}  items={item1} className="shoeCaro"/>
+          
       </div>
 
       <div className="shoes">
-        <h2 className="items">Tech Products</h2>
-        <Carousel breakPoints={breakPoints} emulateTouch={true} useKeyboardArrows={true} stopOnHover={false} dataTouch={true}>
-        {Shoes.map((i) => {
-            return <ShoppingCard key = {i.id} {...i} />;
-          })}
-        </Carousel>
+        <h2 className="items">Stunning Luxurious Shoes</h2>
+        <AliceCarousel
+          responsive={responsive}  items={item2} className="shoeCaro"
+        />
+        
       </div>
       <div className="shoes">
         <h2 className="items">Chocolates</h2>
-        <Carousel breakPoints={breakPoints} emulateTouch={true} useKeyboardArrows={true} stopOnHover={false} dataTouch={true}>
-        {Chocolates.map((i) => {
-            return <ShoppingCard key = {i.id} {...i} />;
-          })}
-        </Carousel>
+        <AliceCarousel
+          responsive={responsive}  items={item3} className="shoeCaro"
+        />
+          
       </div>
-      
     </>
   );
 };
